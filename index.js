a716d8915a936bc6bfaa3f1a76c5e9b7540d9191
kamrulkoche <kamrulkoche@gmail.com>
2024-11-21 01:04:14 +0600

    create reducer for counter

--- index.js
+++ index.js
@@ -29,3 +29,22 @@ const addUser = () => {
         payload: { name: 'shakil' }
     }
 }
+
+// create reducer for counter
+const couterReducer = (state = initialCounterState, action) => {
+
+    switch (action.type) {
+        case INCREMENT:
+            return { ...state, count: state.count + 1 };
+        case DECREMENT:
+            return { ...state, count: state.count - 1 };
+        default:
+            return state;
+
+    }
+}
+
+//1. state
+//2. dispatch action
+//3. reducer
+//4. store
